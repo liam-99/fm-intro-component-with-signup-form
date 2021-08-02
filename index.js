2b1c31df2ea4dc9281fbc5544cdbb43fcb5ccc79
liam.entertainment99@gmail.com <liam.entertainment99@gmail.com>
2021-08-02 11:37:37 +0630

    final result

--- index.js
+++ index.js
@@ -33,9 +33,40 @@ function validateForm() {
                 
                 em.appendChild(document.createTextNode(message));
                 label.appendChild(em);
+                label.parentNode.classList.add("input-box-with-warning");
+            }
+        }
+        else {
+            if(e.id == "mail" && !validateEmail(e.value)) {
+                valid = false;
+                var label = document.getElementById(e.id + "Label");
+                e.value = "";
+                e.setAttribute("placeholder", "example@example.com");
+
+                if(label.hasChildNodes()) {
+                    label.removeChild(label.lastChild);
+                }
+                var em = document.createElement("em");
+                em.appendChild(document.createTextNode("Looks like this is not an email"));
+                label.appendChild(em);
+                label.parentNode.classList.add("input-box-with-warning");
+            }
+            else {
+                e.classList.remove("error-input");
+                var label = document.getElementById(e.id + "Label");
+                if(label.hasChildNodes()) {
+                    label.removeChild(label.lastChild);
+                }
+                label.parentNode.classList.remove("input-box-with-warning");
             }
         }
     }
     
     return valid;
+}
+
+function validateEmail(email) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(email.match(mailformat)) return true;
+    else return false;
 }
